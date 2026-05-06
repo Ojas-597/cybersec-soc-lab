affa8b00a60b2f02790d771c26811de4b2d49153
Ojaswita Ranjit Desai <ojaswitadesai@gmail.com>
2026-05-06 21:01:25 +0530

    Create attacks.js

---
+++ data/attacks.js
@@ -0,0 +1,19 @@
+window.attacks = {
+  "SQL Injection": {
+    severity: "HIGH",
+    def: "Injection of malicious SQL queries",
+    solution: "Use prepared statements"
+  },
+
+  "Port Scanning": {
+    severity: "MEDIUM",
+    def: "Scanning open ports",
+    solution: "Close unused ports"
+  },
+
+  "Phishing": {
+    severity: "HIGH",
+    def: "Fake emails to steal data",
+    solution: "User awareness"
+  }
+};
