61755b385409fb0d0fb5522acf28afa9ce282a3a
Ojaswita Ranjit Desai <ojaswitadesai@gmail.com>
2026-05-07 00:11:09 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 const User = require("../models/User");
-const Log = require("../models/Log");
+const Log = require("../models/Logs");
 
 const { isAuth, isAdmin } = require("../middleware/auth");
 
