eb05be193b0f5f0f55139734ee439ea7bbb34a76
Ojaswita Ranjit Desai <ojaswitadesai@gmail.com>
2026-05-07 12:52:16 +0530

    Update Query.js

--- models/Query.js
+++ models/Query.js
@@ -1,10 +1,27 @@
 const mongoose = require("mongoose");
 
-const schema = new mongoose.Schema({
-  user: String,
-  question: String,
-  response: String,
-  time: { type: Date, default: Date.now }
+const querySchema = new mongoose.Schema({
+
+  user: {
+    type: String,
+    required: true
+  },
+
+  question: {
+    type: String,
+    required: true
+  },
+
+  response: {
+    type: String,
+    required: true
+  },
+
+  time: {
+    type: Date,
+    default: Date.now
+  }
+
 });
 
-module.exports = mongoose.model("Query", schema);
+module.exports = mongoose.model("Query", querySchema);
