fc526b72e82e74d2347c4501190a2594b742dbc9
Ojaswita Ranjit Desai <ojaswitadesai@gmail.com>
2026-05-07 13:24:09 +0530

    Update Logs.js

--- models/Logs.js
+++ models/Logs.js
@@ -1,22 +1,39 @@
 const mongoose = require("mongoose");
 
+
 const logSchema = new mongoose.Schema({
 
   message: {
+
     type: String,
+
     required: true
+
   },
+
 
   level: {
+
     type: String,
+
     default: "INFO"
+
   },
 
+
   time: {
+
     type: Date,
+
     default: Date.now
+
   }
 
 });
 
-module.exports = mongoose.model("Log", logSchema);
+
+module.exports =
+  mongoose.model(
+    "Log",
+    logSchema
+  );
