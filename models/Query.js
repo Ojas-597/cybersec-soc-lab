091a4695098c17552cbdd07552246bf4721a693d
Ojaswita Ranjit Desai <ojaswitadesai@gmail.com>
2026-05-07 13:24:43 +0530

    Update Query.js

--- models/Query.js
+++ models/Query.js
@@ -1,27 +1,48 @@
 const mongoose = require("mongoose");
 
+
 const querySchema = new mongoose.Schema({
 
   user: {
+
     type: String,
+
     required: true
+
   },
+
 
   question: {
+
     type: String,
+
     required: true
+
   },
+
 
   response: {
+
     type: String,
+
     required: true
+
   },
 
+
   time: {
+
     type: Date,
+
     default: Date.now
+
   }
 
 });
 
-module.exports = mongoose.model("Query", querySchema);
+
+module.exports =
+  mongoose.model(
+    "Query",
+    querySchema
+  );
