a0cbd15b11a3109719e537f9abbac9bc5923624c
Ojaswita Ranjit Desai <ojaswitadesai@gmail.com>
2026-05-07 13:19:09 +0530

    Update log.js

--- routes/log.js
+++ routes/log.js
@@ -1,7 +1,11 @@
 const express = require("express");
+
 const Log = require("../models/Logs");
 
-const { isAuth, isAnalyst } = require("../middleware/auth");
+const {
+  isAuth,
+  isAnalyst
+} = require("../middleware/auth");
 
 const router = express.Router();
 
@@ -9,13 +13,17 @@ const router = express.Router();
 /* =========================
    ➕ ADD LOG
 ========================= */
+
 router.post("/log", isAuth, async (req, res) => {
 
   try {
 
     const log = await Log.create({
+
       message: req.body.message,
+
       level: req.body.level || "INFO"
+
     });
 
     res.json({
@@ -35,13 +43,16 @@ router.post("/log", isAuth, async (req, res) => {
 
 
 /* =========================
-   📊 VIEW LOGS
+   📡 GET LOGS
 ========================= */
+
 router.get("/logs", isAuth, isAnalyst, async (req, res) => {
 
   try {
 
-    const logs = await Log.find().sort({ time: -1 });
+    const logs = await Log.find()
+
+      .sort({ time: -1 });
 
     res.json(logs);
 
